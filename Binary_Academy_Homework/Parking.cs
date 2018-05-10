6a6b78027524549fb994e7ba8929503cc670b758
Natalia Proskurnicha <proskurnicha@gmail.com>
2018-05-10 22:20:06 +0300

    Parking class was added and singleton pattern released

---
+++ Binary_Academy_Homework/Parking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Academy_Homework
+{
+    class Parking
+    {
+        private static readonly Lazy<Parking> lazy = new Lazy<Parking>(() => new Parking());
+        //        Parking - данный класс при инициализации использует настройки описанные в классе Settings:
+        public static Parking Instance { get { return lazy.Value; } }
+
+        private Parking()
+        {
+        }
+
+        static List<Car> listCars;
+
+        static List<Transaction> listTransactions;
+
+        static double Balance;
+    }
+}
